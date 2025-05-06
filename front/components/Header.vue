6298165c9e4c091934670cc128177d4be984675a
ytakky2014 <y.takky2014@gmail.com>
2025-05-06 21:26:44 +0900

    avatorを追加

--- front/components/Header.vue
+++ front/components/Header.vue
@@ -1,5 +1,11 @@
 <template>
   <v-app-bar app color="primary" dark>
+    <v-avatar size="48">
+      <v-img
+        alt="John"
+        src="/img/avator.jpg"
+      ></v-img>
+    </v-avatar>
     <v-toolbar-title>info.ytakky.tech</v-toolbar-title>
   </v-app-bar>
 </template>
